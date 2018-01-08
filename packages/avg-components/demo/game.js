43c5d4c2c79910ee9f6b9a54aeb7519e8e8f38d8
Icemic <bingfeng.jw@gmail.com>
2018-01-08 22:20:42 +0800

    update demo

--- packages/avg-components/demo/game.js
+++ packages/avg-components/demo/game.js
@@ -1,11 +1,13 @@
-import { React, Component, core, components } from 'avg-core';
+import { React, Component, core, components, findPixiNode, Surface } from 'avg-core';
 import Image from '../src/lib/image';
 import Container from '../src/lib/container';
 import Layer from '../src/lib/layer';
 import RichText from '../src/lib/richtext';
 import Text from '../src/lib/text';
+import Animation from '../src/lib/animation';
+import Button from '../src/lib/button';
 
-const { Surface } = components;
+// const { Surface } = components;
 
 class Stage extends Component {
   static propTypes = {
@@ -21,9 +23,20 @@ class Stage extends Component {
 }
 
 export default class Game extends Component {
+  state = {
+  }
+  componentDidMount() {
+  }
+  handleClick = (e) => {
+    const node = findPixiNode(this.video);
+    const node2 = findPixiNode(this.video2);
+    node.texture.baseTexture.source.play();
+    node2.texture.baseTexture.source.play();
+  }
   render() {
     return (
       <Stage>
+        {/* <Image position={[0, 0]} src='video/big-buck-bunny_trailer.webm' ref={ref => (this.video2 = ref)} onTap={this.handleClick}/> */}
         <Image src='100x100.png' />
         <Container position={[120, 0]}>
           <Layer width={100} height={100} fillColor={0xffffff} fillAlpha={0.6} clip={true}>
@@ -31,6 +44,11 @@ export default class Game extends Component {
           </Layer>
           <RichText text={'Text 测试'} style={{ fillColor: 'white', fontSize: 26 }} layout={{ gridSize: 26 }} position={[0, 30]} />
         </Container>
+        <Button position={[300, 120]} frames={['animation.png', 8, 1]} />
+        <Animation position={[0, 120]} frames={['animation.png', 8, 1]} loop={true} playing={true} speed={0.125} />
+        <Animation position={[0, 120]} frames={['break.png', 16, 1]} bounce={true} loop={true} playing={true} speed={0.3} />
+        {/* <Image position={[0, 0]} src='video/soccer1.webm' ref={ref => (this.video = ref)} onTap={this.handleClick}/> */}
+        {/* <Animation position={[0, 300]} frames={[1,2,3,4,5,6].map(item => `木头人/${item}.png`)} loop={true} playing={true} speed={0.2} /> */}
       </Stage>
     );
   }
