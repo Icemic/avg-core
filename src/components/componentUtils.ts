a1f0e3e5838fa34ed17f52f78bf51bf47fb6e128
Icemic <bingfeng.jw@gmail.com>
2018-09-01 22:21:32 +0800

    remove useless code

--- src/components/componentUtils.ts
+++
@@ -1,6 +0,0 @@
-import componentify from './pixi/componentify';
-import { mountNode, setValue, updateNode, updateValue } from './pixi/properties';
-
-const tools = { componentify, mountNode, updateNode, setValue, updateValue };
-
-export default tools;
